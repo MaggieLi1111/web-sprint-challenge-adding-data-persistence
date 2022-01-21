62ed6833af0aad79a5dbfa1fb9cf529d47d9ff8f
unknown <lidanyang1111@gmail.com>
2022-01-21 14:22:34 -0800

    version3:stuck on get method

--- api/task/model.js
+++ api/task/model.js
@@ -1,31 +1,32 @@
 // build your `Task` model here
 const db = require("../../data/dbConfig")
 
-async function getAll() {
-    const tasks = await db("tasks as t")
-    .leftjoin("projects as p", "t.project_id", "p.project_id")
-    .select("t.*", "p.project_name", "p.project_description")
-
-    const newTasks = [];
-    for( let i = 0; i < tasks.length ; i++) {
-        let newTask = {
-            task_id: tasks[i].task_id,
-            task_description:tasks[i].task_description,
-            task_notes: tasks[i].task_notes,
-            task_completed: tasks[i].task_completed === 0 ? false : true,
-            project_name: tasks[i].project_name,
-            project_description: tasks[i].project_description
-        }
-        newTasks.push(newTask)
+const transform = (int) => {
+    if (int === 0) {
+        return false;
+    } else {
+        return true;
     }
-    return newTasks;
 }
 
-async function getById (id) {
+async function getAll() {
+    const tasks = await db("tasks as t")
+        .leftjoin("projects as p", "t.project_id", "p.project_id")
+        .select("t.*", "p.project_name", "p.project_description")
+
+    const transformedTasks = tasks.forEach(task => {
+        const newCompleted = transform(task.task_completed);
+        task.task_completed = newCompleted;
+    })
+
+    return transformedTasks;
+}
+
+async function getById(id) {
     const task = await db("tasks").where("task_id", id).first()
     let completed = task.project_completed
     completed === 0 || !completed ? completed = false : completed = true
-    return ({ ...task, task_completed: completed})
+    return ({ ...task, task_completed: completed })
 }
 async function create(newTask) {
     const [task_id] = await db("tasks").insert(newTask)
