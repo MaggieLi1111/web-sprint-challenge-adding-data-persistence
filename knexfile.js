bfa089ffc9efef5a152dcb0ca4bd200823c4acc3
unknown <lidanyang1111@gmail.com>
2022-01-21 08:58:13 -0800

    version1

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,32 @@
-// do not make changes to this file
-const sharedConfig = {
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+ const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
 }
 
 module.exports = {
   development: {
     ...sharedConfig,
     connection: { filename: './data/database.db3' },
-    seeds: { directory: './data/seeds' },
   },
   testing: {
     ...sharedConfig,
-    connection: { filename: './data/test.db3' },
+    connection: { filename: './data/testing.db3' },
   },
-};
+}
